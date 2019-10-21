5ff641ef5775d42dd99635f46c76a23e772af668
camilozky <30737946+camilozky@users.noreply.github.com>
2019-10-21 15:31:38 -0500

    Update Transport.kt

--- app/src/main/java/com/globant/lyfecycles/Transport.kt
+++ app/src/main/java/com/globant/lyfecycles/Transport.kt
@@ -1,5 +1,3 @@
-package com.globant.lyfecycles
-
 abstract class Transport(open var show: String) {
     abstract fun doMove(): String
 }
@@ -9,28 +7,32 @@ open class Road : Transport("Road") {
     override fun doMove() = "Driving"
 }
 
-open class Motorcycle(override var show: String, displacement: String, private var power: String, private var torque: String) : Road()
+open class Motorcycle(
+    override var show: String = "Moto",
+    displacement: String = "N/A",
+    private var power: String = "N/A",
+    private var torque: String = "N/A"
+) : Road()
+
 open class Pulsar(var name: String, var ownerName: String) : Motorcycle(
-        "Pulsar",
-        "199.5 c.c.",
-        "23.5 HP a 9,500 rpm",
-        "18.3 Nm a 8,000 rpm") {
+    "Pulsar",
+    "199.5 c.c.",
+    "23.5 HP a 9,500 rpm",
+    "18.3 Nm a 8,000 rpm"
+) {
     override fun doMove(): String {
         return "verify accelerator " + super.doMove()
     }
 }
 
 open class Boxer(var name: String, var ownerName: String) : Motorcycle(
-        "Boxer",
-        "144.08 cc",
-        "8.83 HP a 7,500 rpm",
-        "12.26 Nm a 4,500 rpm"
+    "Boxer",
+    "144.08 cc",
+    "8.83 HP a 7,500 rpm",
+    "12.26 Nm a 4,500 rpm"
 )
 
-fun main() {
-
-    val polymorphism: Road = Pulsar("Scarlett", "Camilo")
-    val inheritance = Road()
-    println(polymorphism.show)
-    println(inheritance.show)
-}
+val polymorphism: Motorcycle = Pulsar("Scarlett", "Camilo")
+val inheritance: Motorcycle = Motorcycle()
+println(polymorphism.show)
+println(inheritance.show)
